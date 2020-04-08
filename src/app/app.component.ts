0234b7dd0608524bbc707173b0fc23f1d15cc66d
Darantir <yakushev_1996@inbox.ru>
2020-04-08 16:51:30 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,34 @@ import { Product } from './product';
 
 
 export class AppComponent  {
+  products : Product[];
   product: Product;
 
   constructor(){
-    let newProduct = new Product('NICEHAT',
-    'A Nice Black Hat',
-    '/resources/images/products/black-hat.jpg',
-    ['Men','Accesories','Hats'],
-    29.99);
-
-    this.product = newProduct;
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+        new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+        new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hat'],
+        29.99),
+    ];
+  }
+  productWasSelected(product: Product): void
+  {
+    console.log('Product clicked:',product);
   }
 
   name = 'Angular';
