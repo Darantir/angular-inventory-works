f060cb102bb86aaf068e771f45bf4d42bc9fe691
Darantir <yakushev_1996@inbox.ru>
2020-04-08 15:31:02 +0200

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
+
 export class AppComponent  {
+  product: Product;
+
+  constructor(){
+    let newProduct = new Product('NICEHAT',
+    'A Nice Black Hat',
+    '/resources/images/products/black-hat.jpg',
+    ['Men','Accesories','Hats'],
+    29.99);
+
+    this.product = newProduct;
+  }
+
   name = 'Angular';
 }
