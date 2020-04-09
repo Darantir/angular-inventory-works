c14db467692e64c19ca974b4880fa1f04edadd40
Darantir <yakushev_1996@inbox.ru>
2020-04-09 17:21:11 +0200

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductsRowComponent } from './products-row/products-row.component';
+import { ProductsImageComponent } from './products-image/products-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductsListComponent, ProductsRowComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductsListComponent, ProductsRowComponent, ProductsImageComponent, PriceDisplayComponent, ProductRowComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
