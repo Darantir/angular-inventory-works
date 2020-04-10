4f24458a2cbe02e7b0b938b166d0459e7cc397a7
Darantir <yakushev_1996@inbox.ru>
2020-04-10 21:40:54 +0200

    1 file modified, 2 added

---
+++ src/app/products-image/products-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-image',
+  templateUrl: './products-image.component.html',
+  styleUrls: ['./products-image.component.css']
+})
+export class ProductsImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
